c36b97d54ba16a61fe2a96d37d85067335e08489
biradar541 <157600484+biradar541@users.noreply.github.com>
2025-07-07 18:08:04 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -116,7 +116,7 @@ export const login = async (req, res) => {
       .status(200)
       .cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000,
-        httpOnly: true,
+        httpOnly: false,
         sameSite: "None",
       })
       .json({
