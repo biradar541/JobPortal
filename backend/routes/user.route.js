642ef680aa39ab7920ee584aaffe43a8a086a811
biradar541 <157600484+biradar541@users.noreply.github.com>
2025-07-11 15:10:36 +0530

    Update user.route.js

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,11 +2,13 @@ import express from "express"
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { login, logout, register, updateProfile } from "../controllers/user.controller.js"
 import { singleUpload } from "../middlewares/multer.js";
+import { loginLimiter } from "../middlewares/rateLimiter.js"; 
+
 
 const router = express.Router();
 
 router.route("/register").post(singleUpload, register);
-router.route("/login").post(login);
+router.route("/login").post(loginLimiter,login);
 router.route("/logout").get(isAuthenticated,logout);
 router.route("/profile/update").post(isAuthenticated,singleUpload,updateProfile);
 
